7d83fdbcfd8a121aa0c4db93575d196a4818357a
gintares <g.statkute@gmail.com>
2021-03-02 17:48:21 +0000

    Update ptr_charMN.c

--- ptr_charMN.c
+++ ptr_charMN.c
@@ -11,10 +11,25 @@ int main(){
         printf("\n kptr[%d]=%s ", i, kptr[i] );
         //kptr++; 
     } // for ( i=0; i<3; i++ 
-    return 1; 
-}
-
 //RESULT :
 // kptr[0]=A 
 // kptr[1]=B 
 // kptr[2]=C 
+    
+    
+// WORKS WITHOUT MEMORY ALLOCATION, because points to the same memory
+
+ char *keys1[3] = { "AAAAA", "BBBBBBBBB", "CCCCCCCC" }; 
+ void *vkeys1 = &keys1; 
+ char **kptr1 = vkeys1; 
+ for ( i=0; i<3; i++ ) { printf("\n kptr1[%d]=%s ", i, kptr1[i] );  } 
+    
+//RESULTS: 
+ // kptr1[0]=AAAAA 
+ // kptr1[1]=BBBBBBBBB 
+ // kptr1[2]=CCCCCCCC
+    
+    return 1; 
+}
+
+
