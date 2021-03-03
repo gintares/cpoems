efbcb9e05735cd63d27c46ba24070abbc5adf66c
gintares <g.statkute@gmail.com>
2021-03-03 00:16:27 +0000

    Update ptr_charMN.c

--- ptr_charMN.c
+++ ptr_charMN.c
@@ -29,6 +29,21 @@ int main(){
  // kptr1[1]=BBBBBBBBB 
  // kptr1[2]=CCCCCCCC
     
+   //voiding coverts to D1 array of strings 
+    char *keys3[2][2][3] = {
+          {  {"AAA", "BBB", "CCC" }, { "DD", "EE", "FF" }  }, 
+          {  {"GG", "HHH", "JJJ" }, { "KKK", "LLL", "ZZZ" }  }
+    };
+    int i, j, u; 
+    //printf("\n keys[%d][%d][%d]=%s ", 1,2, 1, (char*)keys[1][1][1] ); 
+
+    void *vkeys3 = &keys3;
+    char **kptr3 = vkeys3; // Becomes D1 array 
+    for ( u=0; u<12; u++ ) {  printf(" =%s ", (char*)kptr3[u] ); } //kptr++; 
+    // kptr[u] points to "AAA", "BBB", "CCC", "DD", "EE", "FF" ....
+    // kptr[u][i] points to "AAA" letters from i position
+    // printf &kpt[u][i][j] - does not work, j type is not recognised 
+    
     return 1; 
 }
 
